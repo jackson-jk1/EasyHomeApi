77f2c6f1abf932001b06cd07e07e9361b320936f
jackson-jk1 <jackson.longo.santos@gmail.com>
2022-11-15 15:36:50 -0300

    json return

--- Service/Interfaces/IUserService.cs
+++ Service/Interfaces/IUserService.cs
@@ -11,10 +11,10 @@ namespace Service.Interfaces
     public interface IUserService : IDisposable
     {
         Task<Result<LoginResponse>> Auth(LoginRequest login);
-        Task<Result<string>> Register(UserRequest user);
-        Task<Result<string>> Update(HttpContext context, UserRequest user);
-        Task<Result<string>> UpdatePassword(HttpContext context, PasswordRequest pass);
-        Task<Result<string>> Recover(string email);
+        Task<Result<GenericResponse>> Register(UserRequest user);
+        Task<Result<GenericResponse>> Update(HttpContext context, UserRequest user);
+        Task<Result<GenericResponse>> UpdatePassword(HttpContext context, PasswordRequest pass);
+        Task<Result<GenericResponse>> Recover(string email);
         Task<Result<UserModel>> GetById(int id);
     }
 }
